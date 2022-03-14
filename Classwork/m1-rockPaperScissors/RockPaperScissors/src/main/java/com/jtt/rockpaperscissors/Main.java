1cba5e49ada1f29d7080b9abfb7cccfbf2e133b6
Jennifer.Trongard <jennifer.trongard@gmail.com>
2022-03-14 18:39:56 -0500

    Added case statement for logic of win lose tie

--- Classwork/m1-rockPaperScissors/RockPaperScissors/src/main/java/com/jtt/rockpaperscissors/Main.java
+++ Classwork/m1-rockPaperScissors/RockPaperScissors/src/main/java/com/jtt/rockpaperscissors/Main.java
@@ -7,7 +7,6 @@ package com.jtt.rockpaperscissors;
 import java.util.Scanner;
 import java.util.Random;
 
-
 /**
  *
  * @author Jenny
@@ -25,9 +24,10 @@ public class Main {
         int numGames = 0;
         int numComputerWins = 0;
         int numHumanWins = 0;
+        int ties = 0;
         try {
             System.out.println("Welcome to Rock Paper Scissors.");
-            System.out.print("How Many games do you want to play 1-10:");
+            System.out.println("How Many games do you want to play 1-10:");
             String numGamesStr = input.nextLine();
             numGames = Integer.parseInt(numGamesStr);
             if (numGames < 1 || numGames > 10) {
@@ -46,21 +46,65 @@ public class Main {
                 System.out.println("2 Paper");
                 System.out.println("3 Scissors");
                 System.out.println("***************************");
-                System.out.print("Enter Choice 1-3:");
+                System.out.println("Enter Choice 1-3:");
                 String choiceStr = input.nextLine();
                 int choice = Integer.parseInt(choiceStr);
                 if (choice < 1 || choice > 3) {
                     throw new Exception();
                 }
-                int compChoice= randNumber.nextInt(3)+1;
-                
+                int compChoice = randNumber.nextInt(3) + 1;
+                switch (compChoice) {
+                    case 1: // rock
+                        System.out.println(" Computer picks 1 Rock");
+                        if (choice == 1) {
+                            ties++;
+                            System.out.println("Game is a tie");
+                        } else if (choice == 2) {
+                            numHumanWins++;
+                            System.out.println("Human wins");
+                        } else {
+                            numComputerWins++;
+                            System.out.println("Computer wins");
+                        }
+                        break;
+                    case 2:  // paper
+                        System.out.println(" Computer picks 2 Paper");
+                         if (choice == 2) {
+                            ties++;
+                            System.out.println("Game is a tie");
+                        } else if (choice == 3) {
+                            numHumanWins++;
+                            System.out.println("Human wins");
+                        } else {
+                            numComputerWins++;
+                            System.out.println("Computer wins");
+                        }                       
+                        break;
+                    case 3:  // scissors
+                        System.out.println(" Computer picks 3 Scissors");
+                          if (choice == 3) {
+                            ties++;
+                            System.out.println("Game is a tie");
+                        } else if (choice == 1) {
+                            numHumanWins++;
+                            System.out.println("Human wins");
+                        } else {
+                            numComputerWins++;
+                            System.out.println("Computer wins");
+                        }                           
+                        break;
+                    default:
+                } // switch
 
-            }// end try
-            catch (Exception x) {
+
+        }// end try
+        catch (Exception x) {
                 System.out.println("invalid input.");
                 i--; // dont increment game.
             }
-        } // end for
+    } // end for
+        System.out.println("Computer won "+ numComputerWins +" times.");
+        System.out.println("Human won "+ numHumanWins +" times.");
 
     } // end main
 } // end class
