3c2a62f0f64c952cdfab8acdd510cd4f40492d13
Jennifer Trongard <jennifer.trongard@gmail.com>
2023-05-29 19:19:48 -0500

    removed imports

--- Classwork/SuperHeroSightingsDataLayer/NBSH/src/main/java/sg/jst/nb_superhero/NB_SuperHero.java
+++ Classwork/SuperHeroSightingsDataLayer/NBSH/src/main/java/sg/jst/nb_superhero/NB_SuperHero.java
@@ -6,10 +6,6 @@ package sg.jst.nb_superhero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-import java.time.LocalDateTime;
-import java.util.List;
     
 /**
  *
