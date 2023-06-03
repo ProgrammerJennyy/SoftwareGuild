6edc5a1c8eb0c78994c0a6ee8a88c87e040fc5c5
Jennifer Trongard <jennifer.trongard@gmail.com>
2023-06-03 18:24:46 -0500

    added superpower select box

--- Classwork/SuperHeroSightingWebApp/SuperSightingDatabase/src/main/java/sg/jst/superSightingsDatabase/controller/superHeros.java
+++ Classwork/SuperHeroSightingWebApp/SuperSightingDatabase/src/main/java/sg/jst/superSightingsDatabase/controller/superHeros.java
@@ -6,7 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import sg.jst.superSightingsDatabase.DAO.SuperHeroDAOImp;
+import sg.jst.superSightingsDatabase.DAO.SuperPowerDAOImp;
 import sg.jst.superSightingsDatabase.DTO.SuperHeroDTO;
+import sg.jst.superSightingsDatabase.DTO.superPowerDTO;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -16,20 +18,32 @@ public class superHeros {
 
     @Autowired
     SuperHeroDAOImp superHeroDAOImp;
+
+    @Autowired
+    SuperPowerDAOImp superPowerDAOImp;
+
     @GetMapping("superHeros")
     public String displayTeachers(Model model) {
         List<SuperHeroDTO> dtos = superHeroDAOImp.ReadAll();
+        for(SuperHeroDTO dto: dtos)
+        {
+            superPowerDTO retval;
+            retval=superPowerDAOImp.GetSuperPowerById(dto.getSuperPowerId());
+            dto.setSuperPower(retval.getSuperPowerName());
+        }
         model.addAttribute("superHeros", dtos);
+        List<superPowerDTO> spDtos = superPowerDAOImp.ReadAll();
+        model.addAttribute("superPowerId",spDtos);
         return "superHeros";
     }
     @PostMapping("addSuper")
     public String addSuper(HttpServletRequest request) {
         String superName = request.getParameter("superName");
-        String superPower = request.getParameter("superPower");
+        String superPowerId = request.getParameter("superPowerId");
         String description = request.getParameter("description");
         SuperHeroDTO newSuper = new SuperHeroDTO();
         newSuper.setName(superName);
-        newSuper.setSuperPower(superPower);
+        newSuper.setSuperPowerId(Integer.parseInt(superPowerId));
         newSuper.setDescription(description);
         superHeroDAOImp.CreateSuperHero(newSuper);
         return "redirect:superHeros";
