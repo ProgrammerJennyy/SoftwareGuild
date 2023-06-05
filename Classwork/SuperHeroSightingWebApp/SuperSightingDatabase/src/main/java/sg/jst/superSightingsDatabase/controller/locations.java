d87175e517a8f4e2bc4d036927a321343df79bb3
Jennifer Trongard <jennifer.trongard@gmail.com>
2023-06-05 18:27:44 -0500

    Added edit location

--- Classwork/SuperHeroSightingWebApp/SuperSightingDatabase/src/main/java/sg/jst/superSightingsDatabase/controller/locations.java
+++ Classwork/SuperHeroSightingWebApp/SuperSightingDatabase/src/main/java/sg/jst/superSightingsDatabase/controller/locations.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import sg.jst.superSightingsDatabase.DAO.sightingLocationDAOImp;
 import sg.jst.superSightingsDatabase.DTO.SuperHeroDTO;
 import sg.jst.superSightingsDatabase.DTO.sightingLocationDTO;
+import sg.jst.superSightingsDatabase.DTO.superPowerDTO;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -55,5 +56,45 @@ public class locations {
         SightingLocationDAOImp.DeletesightingLocation(dto);
         return "redirect:/locations";
     }
+    @GetMapping("editSL")
+    public String editSLs(HttpServletRequest request, Model model) {
+        int id = Integer.parseInt(request.getParameter("SL_ID"));
+        sightingLocationDTO dto = SightingLocationDAOImp.GetsightingLocationId(id);
+        model.addAttribute("SL_ID", id);
+        model.addAttribute("Name", dto.getName());
+        model.addAttribute("Description", dto.getDescription());
+        model.addAttribute("Address", dto.getAddress());
+        model.addAttribute("City", dto.getCity());
+        model.addAttribute("Latitude", dto.getLatitude());
+        model.addAttribute("Longitude", dto.getLongitude());
+        model.addAttribute("State", dto.getState());
+        model.addAttribute("Zip", dto.getZip());
+        return "editLocations";
+    }
+
+    @PostMapping("EditLocation")
+    public String updateLocation(HttpServletRequest request) {
+        String Name = request.getParameter("Name");
+        String Description = request.getParameter("Description");
+        String Address = request.getParameter("Address");
+        String City = request.getParameter("City");
+        String State = request.getParameter("State");
+        String Zip = request.getParameter("Zip");
+        String Longitude = request.getParameter("Longitude");
+        String Latitude = request.getParameter("Latitude");
+        int id = Integer.parseInt(request.getParameter("SL_ID"));
+        sightingLocationDTO dto = SightingLocationDAOImp.GetsightingLocationId(id);
+        dto.setName(Name);
+        dto.setDescription(Description);
+        dto.setAddress(Address);
+        dto.setCity(City);
+        dto.setState(State);
+        dto.setZip(Zip);
+        dto.setLatitude(Double.parseDouble(Latitude));
+        dto.setLongitude(Double.parseDouble(Longitude));
+        SightingLocationDAOImp.CreatesightingLocation(dto);
+        return "redirect:/locations";
+    }
+
 
 }
