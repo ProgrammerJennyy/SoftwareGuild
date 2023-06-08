207c5439c9b080932e3eabe4d07f0245dc007907
Jennifer Trongard <jennifer.trongard@gmail.com>
2023-06-07 22:23:16 -0500

    did power and edit power validation

--- Classwork/SuperHeroSightingWebApp/SuperSightingDatabase/src/main/java/sg/jst/superSightingsDatabase/DTO/superPowerDTO.java
+++ Classwork/SuperHeroSightingWebApp/SuperSightingDatabase/src/main/java/sg/jst/superSightingsDatabase/DTO/superPowerDTO.java
@@ -1,6 +1,12 @@
 package sg.jst.superSightingsDatabase.DTO;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class superPowerDTO {
+
+
     private int superPowerId;
 
     public String getSuperPowerName() {
@@ -10,7 +16,9 @@ public class superPowerDTO {
     public void setSuperPowerName(String superPowerName) {
         this.superPowerName = superPowerName;
     }
-
+    @NotBlank(message = "Power name cannot be blank.")
+    @Size(max = 45, message = "The name of the power must be 45 characters or less.")
+    @Pattern(regexp = "^[a-zA-Z\\s]+$", message = "Powers cannot contain a number.")
     private String superPowerName;
 
     public int getSuperPowerId() {
--- Classwork/SuperHeroSightingWebApp/SuperSightingDatabase/src/main/java/sg/jst/superSightingsDatabase/controller/superPowers.java
+++ Classwork/SuperHeroSightingWebApp/SuperSightingDatabase/src/main/java/sg/jst/superSightingsDatabase/controller/superPowers.java
@@ -3,6 +3,7 @@ package sg.jst.superSightingsDatabase.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import sg.jst.superSightingsDatabase.DAO.SuperPowerDAOImp;
@@ -10,7 +11,17 @@ import sg.jst.superSightingsDatabase.DTO.SuperHeroDTO;
 import sg.jst.superSightingsDatabase.DTO.superPowerDTO;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolation;
+import javax.validation.Valid;
+import javax.validation.Validator;
+import javax.validation.Validation;
+
+
+
 
 @Controller
 public class superPowers {
@@ -18,10 +29,13 @@ public class superPowers {
     @Autowired
     SuperPowerDAOImp superPowerDAOImp;
 
+    Set<ConstraintViolation<superPowerDTO>> violations = new HashSet();
+
     @GetMapping("superPowers")
     public String displayPowers(Model model) {
         List<superPowerDTO> dtos = superPowerDAOImp.ReadAll();
         model.addAttribute("superPowers", dtos);
+        model.addAttribute("errors", violations);
         return "superPowers";
     }
 
@@ -30,7 +44,11 @@ public class superPowers {
         String superName = request.getParameter("superPowerName");
         superPowerDTO dto = new superPowerDTO();
         dto.setSuperPowerName(superName);
-        superPowerDAOImp.CreateSuperPower(dto);
+        Validator validate = Validation.buildDefaultValidatorFactory().getValidator();
+        violations = validate.validate(dto);
+        if (violations.isEmpty()) {
+            superPowerDAOImp.CreateSuperPower(dto);
+        }
         return "redirect:/superPowers";
     }
 
@@ -48,18 +66,39 @@ public class superPowers {
         superPowerDTO dto = superPowerDAOImp.GetSuperPowerById(id);
         model.addAttribute("SuperPowerName", dto.getSuperPowerName());
         model.addAttribute("SuperPowerId", dto.getSuperPowerId());
-        return "editSuperPowers";
+        Validator validate = Validation.buildDefaultValidatorFactory().getValidator();
+        violations = validate.validate(dto);
+        model.addAttribute("errors", violations);
+         return "editSuperPowers";
     }
+
+
+//    @PostMapping("EditPower")
+//    public String editPower(HttpServletRequest request) {
+//        String superPowerName = request.getParameter("SuperPowerName");
+//        String superPowerId = request.getParameter("SuperPowerId");
+//        int id = Integer.parseInt(request.getParameter("SuperPowerId"));
+//        superPowerDTO dto = superPowerDAOImp.GetSuperPowerById(id);
+//        dto.setSuperPowerName(superPowerName);
+//        superPowerDAOImp.UpdateSuperPower(dto);
+//        return "redirect:/superPowers";
+//    }
 
     @PostMapping("EditPower")
-    public String editPower(HttpServletRequest request) {
-        String superPowerName = request.getParameter("SuperPowerName");
-        String superPowerId = request.getParameter("SuperPowerId");
-        int id = Integer.parseInt(request.getParameter("SuperPowerId"));
-        superPowerDTO dto = superPowerDAOImp.GetSuperPowerById(id);
-        dto.setSuperPowerName(superPowerName);
-        superPowerDAOImp.UpdateSuperPower(dto);
-        return "redirect:/superPowers";
-    }
+    public String performEditStudent(@Valid superPowerDTO dto, BindingResult result,HttpServletRequest request,Model model) {
+        Validator validate = Validation.buildDefaultValidatorFactory().getValidator();
+        violations = validate.validate(dto);
+        if (violations.isEmpty()) {
+            superPowerDAOImp.UpdateSuperPower(dto);
+            return "redirect:/superPowers";
+        }
+        else
+        {
+            model.addAttribute("errors", violations);
+            model.addAttribute("SuperPowerName", dto.getSuperPowerName());
+            model.addAttribute("SuperPowerId", dto.getSuperPowerId());
+            return "editSuperPowers";
+        }
+     }
 
-}
+} // end class
