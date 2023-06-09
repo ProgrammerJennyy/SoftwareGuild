90474cff66f4dec79f55f8dd4677c5ac70a725e2
Jennifer Trongard <jennifer.trongard@gmail.com>
2023-06-09 12:00:02 -0500

    did location validation

--- Classwork/SuperHeroSightingWebApp/SuperSightingDatabase/src/main/java/sg/jst/superSightingsDatabase/DTO/sightingLocationDTO.java
+++ Classwork/SuperHeroSightingWebApp/SuperSightingDatabase/src/main/java/sg/jst/superSightingsDatabase/DTO/sightingLocationDTO.java
@@ -1,14 +1,43 @@
 package sg.jst.superSightingsDatabase.DTO;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class sightingLocationDTO {
     private int SL_ID;
+
+    @NotBlank(message = "Name cannot be blank.")
+    @Size(max = 45, message = "Name  must be 45 characters or less.")
     private String Name;
+
+    @NotBlank(message = "Description cannot be blank.")
+    @Size(max = 200, message = "Description  must be 200 characters or less.")
     private String Description;
+
+    @NotBlank(message = "Address cannot be blank.")
+    @Size(max = 200, message = "Address  must be 200 characters or less.")
     private String Address;
+
+    @NotBlank(message = "City cannot be blank.")
+    @Size(max = 45, message = "City  must be 45 characters or less.")
     private String City;
+
+    @NotBlank(message = "State cannot be blank.")
+    @Size(max = 45, message = "State  must be 45 characters or less.")
     private String State;
+
+    @NotBlank(message = "Zip cannot be blank.")
+    @Size(max = 45, message = "Zip  must be 45 characters or less.")
     private String Zip;
+
+    @NotNull(message = "Latitude cannot be blank.")
+    @Digits(integer = 10, fraction = 8, message = "Latitude has to be a valid coordinate.")
     private double Latitude;
+
+    @NotNull(message = "Longitude cannot be blank.")
+    @Digits(integer = 10, fraction = 8, message = "Longitude has to be a valid coordinate.")
     private double Longitude;
 
     public int getSL_ID() {
--- Classwork/SuperHeroSightingWebApp/SuperSightingDatabase/src/main/java/sg/jst/superSightingsDatabase/controller/locations.java
+++ Classwork/SuperHeroSightingWebApp/SuperSightingDatabase/src/main/java/sg/jst/superSightingsDatabase/controller/locations.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import sg.jst.superSightingsDatabase.DAO.sightingLocationDAOImp;
@@ -12,7 +13,13 @@ import sg.jst.superSightingsDatabase.DTO.sightingLocationDTO;
 import sg.jst.superSightingsDatabase.DTO.superPowerDTO;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolation;
+import javax.validation.Valid;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Controller
 public class locations {
@@ -20,10 +27,14 @@ public class locations {
     @Autowired
     sightingLocationDAOImp SightingLocationDAOImp;
 
+    Set<ConstraintViolation<sightingLocationDTO>> violations = new HashSet();
+
+
     @GetMapping("locations")
     public String loadpage(Model model) {
         List<sightingLocationDTO> dtos = SightingLocationDAOImp.ReadAll();
         model.addAttribute("sLocations", dtos);
+        model.addAttribute("errors", violations);
         return "locations";
     }
     @PostMapping("addLocation")
@@ -43,9 +54,27 @@ public class locations {
         dto.setCity(City);
         dto.setState(State);
         dto.setZip(Zip);
-        dto.setLatitude(Double.parseDouble(Latitude));
-        dto.setLongitude(Double.parseDouble(Longitude));
-        SightingLocationDAOImp.CreatesightingLocation(dto);
+        if(!Latitude.isEmpty()) {
+            try {
+                dto.setLatitude(Double.parseDouble(Latitude));
+            }
+            catch(Exception e){
+                // error happened
+            }
+        }
+        if(!Longitude.isEmpty()) {
+            try {
+                dto.setLongitude(Double.parseDouble(Longitude));
+            }
+            catch(Exception e){
+                // error happened
+            }
+        }
+        Validator validate = Validation.buildDefaultValidatorFactory().getValidator();
+        violations = validate.validate(dto);
+        if (violations.isEmpty()) {
+            SightingLocationDAOImp.CreatesightingLocation(dto);
+        }
         return "redirect:/locations";
     }
 
@@ -69,31 +98,38 @@ public class locations {
         model.addAttribute("Longitude", dto.getLongitude());
         model.addAttribute("State", dto.getState());
         model.addAttribute("Zip", dto.getZip());
+        Validator validate = Validation.buildDefaultValidatorFactory().getValidator();
+        violations = validate.validate(dto);
+        model.addAttribute("errors", violations);
         return "editLocations";
     }
 
     @PostMapping("EditLocation")
-    public String updateLocation(HttpServletRequest request) {
-        String Name = request.getParameter("Name");
-        String Description = request.getParameter("Description");
-        String Address = request.getParameter("Address");
-        String City = request.getParameter("City");
-        String State = request.getParameter("State");
-        String Zip = request.getParameter("Zip");
-        String Longitude = request.getParameter("Longitude");
-        String Latitude = request.getParameter("Latitude");
-        int id = Integer.parseInt(request.getParameter("SL_ID"));
-        sightingLocationDTO dto = SightingLocationDAOImp.GetsightingLocationId(id);
-        dto.setName(Name);
-        dto.setDescription(Description);
-        dto.setAddress(Address);
-        dto.setCity(City);
-        dto.setState(State);
-        dto.setZip(Zip);
-        dto.setLatitude(Double.parseDouble(Latitude));
-        dto.setLongitude(Double.parseDouble(Longitude));
-        SightingLocationDAOImp.UpdatesightingLocation(dto);
-        return "redirect:/locations";
+    public String updateLocation(@Valid sightingLocationDTO dto, BindingResult result, HttpServletRequest request, Model model) {
+        Validator validate = Validation.buildDefaultValidatorFactory().getValidator();
+        violations = validate.validate(dto);
+        if (violations.isEmpty()) {
+            SightingLocationDAOImp.UpdatesightingLocation(dto);
+            return "redirect:/locations";
+        }
+        else
+        {
+            model.addAttribute("SL_ID", dto.getSL_ID());
+            model.addAttribute("Name", dto.getName());
+            model.addAttribute("Description", dto.getDescription());
+            model.addAttribute("Address", dto.getAddress());
+            model.addAttribute("City", dto.getCity());
+            model.addAttribute("Latitude", dto.getLatitude());
+            model.addAttribute("Longitude", dto.getLongitude());
+            model.addAttribute("State", dto.getState());
+            model.addAttribute("Zip", dto.getZip());
+            model.addAttribute("errors", violations);
+            return "editLocations";
+        }
+
+
+
+
     }
 
 
